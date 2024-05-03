8e705c8cb3db7bb7131a908a8dc0ee5e02008b63
Leonie <ngwendoline2@gmail.com>
2024-05-02 22:05:48 -0400

    about and contact page running

--- react-hooks/src/pages/ContactPage.jsx
+++ react-hooks/src/pages/ContactPage.jsx
@@ -2,8 +2,20 @@ import React from 'react';
 import Calendar from 'react-calendar';
 
 
-const ContactPage = () => 
-<div>Contact Page</div>;
-<div>calendar</div>;
+const ContactPage = () => {
+    return (
+        <>
+        <div className='Contact'>You can get to us through</div>;
+        <b></b>
+        <p>phone: 000 812 4060</p>
+        <br></br>
+        <p>email: joyjoyjoy@peace.com</p>
+        <br></br>
+        <div>
+            <button id='btn'>Calenda</button>
+        </div>
+        </>
+    )
+}
 
 export default ContactPage;
--- react-hooks/src/pages/HomePage.jsx
+++ react-hooks/src/pages/HomePage.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
 
-const HomePage = () => <div>Welcome home, the city of love</div>;
+const HomePage = () => {
+    return (
+        <>
+            <div className='Home'>Welcome home, the city of love</div>;
+            <br></br>
+            <div>
+                <button id='btn'>LOGIN</button>
+            </div>
+        </>
+    )
+
+}
 
 
 export default HomePage;
